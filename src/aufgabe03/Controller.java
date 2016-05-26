2d318d815adb51394585b71b2593ac8b0d4c3f6f
Lydia <lydia.pflug@web.de>
2016-05-26 18:35:46 +0200

    neuer Versuch

--- src/aufgabe03/Controller.java
+++ src/aufgabe03/Controller.java
@@ -1,5 +1,19 @@
 package aufgabe03;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
+public class Controller {
+	@FXML
+	private Label label1;
+	
+	@FXML
+	public void buttonPressed() {
+		label1.setText("Button wurde gedrückt!");
+	}
+	
+	@FXML
+	public void buttonPressed2() {
+		label1.setText(" ");
+	}
 }
--- src/aufgabe03/Main.java
+++ src/aufgabe03/Main.java
@@ -1,23 +1,26 @@
 package aufgabe03;
-
+	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Pane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
